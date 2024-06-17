a7ecfb8e9bd2a09eb013222bd38f89d741af17da
zhutmost <zhutmost@outlook.com>
2024-06-17 20:28:21 +0800

    feat: remove cache support

--- main.js
+++ main.js
@@ -5,47 +5,18 @@ const tc = require('@actions/tool-cache');
 const fs = require('fs');
 
 async function main() {
-  const millScriptPath = '.mill-bin'
+  const millPath = '.mill-bin'
 
   try {
-    const defaultMillVersion = core.getInput('default-mill-version');
-    const userMillVersion = core.getInput('mill-version');
-    core.exportVariable('DEFAULT_MILL_VERSION', defaultMillVersion);
-    if (userMillVersion === '-1') {
-      core.exportVariable('MILL_VERSION', userMillVersion);
-    }
+    const millVersion = core.getInput('mill-version');
 
-    // var millVersion = defaultMillVersion;
-    // if (userMillVersion === '-1') {
-    //   millVersion = userMillVersion;
-    // } else {
-    //   if (fs.existsSync(".mill-version")) {
-    //     millVersion = fs.readFileSync(".mill-version").split('\n')[0].trim();
-    //   } else if (fs.existsSync(".config/mill-version")) {
-    //     millVersion = fs.readFileSync(".config/mill-version").split('\n')[0].trim();
-    //   }
-    // }
-
-    var millBinaryCachedPath = tc.find('millBinary', "0");
-    if (millBinaryCachedPath) {
-      core.info(`millBinaries found in cache ${millBinaryCachedPath}`);
-    }
-
-    var millScriptCachedPath = tc.find('millScript', defaultMillVersion);
-    if (millScriptCachedPath) {
-      core.info(`millScript found in cache ${millScriptCachedPath}`);
-    } else {
-      const millScriptDownloadPath = await tc.downloadTool(`https://github.com/lihaoyi/mill/releases/download/${defaultMillVersion}/${defaultMillVersion}`);
-      await io.mkdirP(millScriptPath);
-      await io.cp(millScriptDownloadPath, `${millScriptPath}/mill`, { force: true });
-      fs.chmodSync(`${millScriptPath}/mill`, '0755');
-      millScriptCachedPath = await tc.cacheDir(millScriptPath, 'millScript', defaultMillVersion);
-    }
-    core.addPath(millScriptCachedPath);
+    const millDownloadPath = await tc.downloadTool(`https://github.com/lihaoyi/mill/releases/download/${millVersion}/${millVersion}`);
+    await io.mkdirP(millPath);
+    await io.cp(millDownloadPath, `${millPath}/mill`, { force: true });
+    fs.chmodSync(`${millPath}/mill`, '0755');
+    core.addPath(millPath);
 
     await exec.exec('mill', ['version']);
-
-    await tc.cacheDir(millBinaryCachedPath, 'millBinary', "0")
   } catch (error) {
     core.setFailed(error.message);
   }
