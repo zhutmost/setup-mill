cb61d29e37420cf60fea3ea102be9805dbd86186
zhutmost <zhutmost@outlook.com>
2025-06-20 22:57:42 +0800

    feat: prepare for support of mill v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as core from '@actions/core'
 import * as io from '@actions/io'
 import * as exec from '@actions/exec'
 import * as tc from '@actions/tool-cache'
+import { compareVersions } from 'compare-versions'
 
 async function main(): Promise<void> {
   const millBinPath = path.join(process.cwd(), 'mill_bin')
@@ -12,9 +13,12 @@ async function main(): Promise<void> {
     core.info('Installing mill ...')
 
     const millVersion: string = core.getInput('mill-version')
-    const millDownloadPath: string = await tc.downloadTool(
-      `https://github.com/lihaoyi/mill/releases/download/${millVersion}/${millVersion}`
-    )
+
+    const millUrl: string = compareVersions(millVersion, '0.12.6', '>=')
+      ? `https://repo1.maven.org/maven2/com/lihaoyi/mill-dist/${millVersion}/mill-dist-${millVersion}-mill.sh`
+      : `https://github.com/lihaoyi/mill/releases/download/${millVersion}/${millVersion}`
+
+    const millDownloadPath: string = await tc.downloadTool(millUrl)
     await io.mkdirP(millBinPath)
     await io.cp(millDownloadPath, `${millBinPath}/mill`, { force: true })
     fs.chmodSync(`${millBinPath}/mill`, '0755')
